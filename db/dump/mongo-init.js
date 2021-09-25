72bdd8427d1394470844f590bb42b439cae1647f
mmondani <mmondani89@gmail.com>
2021-09-25 13:03:24 -0300

    Se agrega el servicio de la API en el docker-compose y en el script de inicialización de mongodb, se agrega la inserción del usuario admin en la collection iotUser

--- db/dump/mongo-init.js
+++ db/dump/mongo-init.js
@@ -17,3 +17,6 @@ db.createCollection("iotDato");
 db.createCollection("iotDisp");
 db.createCollection("iotEmpresa");
 db.createCollection("iotUser");
+
+// Se agrega el usuario admin a la fuerza. El password se obtuvo encriptando admin123 en la web https://bcrypt-generator.com/
+db.iotUser.insertOne({name: "admin", email:"admin@admin.com", password: "$2a$10$iyaA/ZYLqWyvqWhQUo45eusRWhfP24t5v9JdEMnvnHABI77juFMaq", rol: "admin"});
